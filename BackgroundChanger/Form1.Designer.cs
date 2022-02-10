59edeb6c3fc4ce747da396d6c61425108df945ff
rezne <rezne@ZVERY>
2022-02-10 21:25:19 +0000

    Add project files.

---
+++ BackgroundChanger/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace BackgroundChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartConverting = new System.Windows.Forms.Button();
+            this.changingBackground = new System.Windows.Forms.CheckBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartConverting
+            // 
+            this.StartConverting.Location = new System.Drawing.Point(302, 130);
+            this.StartConverting.Name = "StartConverting";
+            this.StartConverting.Size = new System.Drawing.Size(250, 150);
+            this.StartConverting.TabIndex = 0;
+            this.StartConverting.Text = "Start Converting";
+            this.StartConverting.UseVisualStyleBackColor = true;
+            this.StartConverting.Click += new System.EventHandler(this.StartConverting_Click);
+            // 
+            // changingBackground
+            // 
+            this.changingBackground.AutoSize = true;
+            this.changingBackground.Location = new System.Drawing.Point(359, 350);
+            this.changingBackground.Name = "changingBackground";
+            this.changingBackground.Size = new System.Drawing.Size(132, 17);
+            this.changingBackground.TabIndex = 1;
+            this.changingBackground.Text = "Changing Background";
+            this.changingBackground.UseVisualStyleBackColor = true;
+            this.changingBackground.CheckedChanged += new System.EventHandler(this.changingBackground_CheckedChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" +
+    "s (*.*)|*.*";
+            // 
+            // openFile
+            // 
+            this.openFile.Location = new System.Drawing.Point(593, 208);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(120, 72);
+            this.openFile.TabIndex = 2;
+            this.openFile.Text = "Open Photo";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.changingBackground);
+            this.Controls.Add(this.StartConverting);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartConverting;
+        private System.Windows.Forms.CheckBox changingBackground;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFile;
+    }
+}
+
---
+++ BackgroundChanger/Form1.cs
@@ -0,0 +1,109 @@
+﻿using BackgroundChanger.Properties;
+using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BackgroundChanger
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
+
+        private static readonly UInt32 SPI_SETDESKWALLPAPER = 0x14;
+        private static readonly UInt32 SPIF_UPDATEINIFILE = 0x01;
+        private static readonly UInt32 SPIF_SENDWININICHANGE = 0x02;
+
+        bool allowToChangePicture = false;
+
+        string pathToPhoto;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartConverting_Click(object sender, EventArgs e)
+        {
+            convertingImage();
+
+            changingWindowsBackground(@"C:\Users\rezne\Desktop\dynamic wallpapers\anime.heic", true);
+        }
+
+        private void changingWindowsBackground(string path, bool update_registry)
+        {
+
+
+            try
+            {
+
+                // Set the desktop background to this file.
+                if (allowToChangePicture)
+                {
+
+                    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path,
+                        SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+
+                    MessageBox.Show("Background image changed",
+                        "Operation successful", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error changing picture" +
+                    path + ".\n" + ex.Message,
+                    "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void convertingImage()
+        {
+
+            using (var images = new MagickImageCollection())
+            {
+
+                images.Read(@"C:\Users\rezne\Desktop\dynamic wallpapers\anime.heic");
+
+                var page = 1;
+                foreach (var image in images)
+                {
+                    // Write page to file that contains the page number
+                    image.Write(@"C:\Users\rezne\Desktop\dynamic wallpapers\Anime.Page" + page + ".jpg");
+                    page++;
+                }
+
+            }
+        }
+
+        private void changingBackground_CheckedChanged(object sender, EventArgs e)
+        {
+            if (changingBackground.Checked)
+            {
+                allowToChangePicture = true;
+            }
+
+        }
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathToPhoto.Load(openFileDialog1.FileName);
+            }
+        }
+    }
+}
